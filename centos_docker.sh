268d17663080cc2e9e7b70147bb9e6f583aa8a3f
reaperhero <43801667+reaperhero@users.noreply.github.com>
2019-04-07 00:13:48 +0800

    Create centos_docker.sh

---
+++ centos_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum makecache fast
+yum -y install docker-ce
+systemctl start docker
+systemctl enable docker
+echo "==========================="
+echo "docker run hello-world"
+echo "==========================="
