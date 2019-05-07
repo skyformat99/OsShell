edf61c09e70017670468525c3b94ab17d51c8a0a
reaperhero <43801667+reaperhero@users.noreply.github.com>
2019-05-07 22:59:53 +0800

    Create go1.12.5.sh

---
+++ go1.12.5.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir /tmp/go && cd /tmp/go
+wget https://golang.google.cn/doc/install?download=go1.12.5.linux-amd64.tar.gz
+tar -zxvf go1.12.5.linux-amd64.tar.gz && sudo mv go /usr/local
+sudo apt-get purge golang-go
+cat << EOF >>　~/.zshrc
+export GOROOT=/usr/local/go
+export GOPATH=/home/chen/hello
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+EOF
+cat << EOF >>　~/.profile
+export GOROOT=/usr/local/go
+export GOPATH=/home/chen/hello
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+EOF
+
+source ~/.profile
+source ~/.zshrc
+
+
