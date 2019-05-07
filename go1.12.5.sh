8b5e60f54ce20cb30762c5eb2177565e5bee3aa8
reaperhero <43801667+reaperhero@users.noreply.github.com>
2019-05-07 23:54:14 +0800

    Update go1.12.5.sh

--- go1.12.5.sh
+++ go1.12.5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir /tmp/go && cd /tmp/go
 wget https://golang.google.cn/doc/install?download=go1.12.5.linux-amd64.tar.gz
-# wget https://golang.google.cn/doc/install?download=go1.10.2.linux-arm64.tar.gz
+# wget https://golang.google.cn/doc/install?download=go1.10.2.linux-amd64.tar.gz
 tar -zxvf go1.12.5.linux-amd64.tar.gz && sudo mv go /usr/local
 sudo apt-get purge golang-go
 cat << EOF >>　~/.zshrc
